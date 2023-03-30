2a8edfa4514dcda9e75afc9b5211761ea7b2409b
naderKhaddaj <126360542+naderKhaddaj@users.noreply.github.com>
2023-03-30 17:14:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+
+
+
+
+
+
+
+window.addEventListener("scroll", reveal);
